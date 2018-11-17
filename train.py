bd91a1da70a3c490462ed034728147753a69daea
Nickolas Westman <westman4@illinois.edu>
2018-11-17 11:05:46 -0800

    Use config over hand-grown options

--- train.py
+++ train.py
@@ -1,9 +1,27 @@
 from lib.data import Conll_dataset
+from itertools import count
+import os
+import numpy as np
+from config import opt, additional_args
 import logging
+import time
+start_time = time.time()
+additional_args(opt)
+N_SAMPLES = opt.nsamples
+
+logging.basicConfig(
+    filename=os.path.join(
+        opt.save_dir,
+        opt.al_method +
+        'only.log') if opt.logfolder else None,
+    level=logging.INFO)
+logging.getLogger("data").setLevel('WARNING')
+logging.getLogger("model").setLevel('WARNING')
+logging.getLogger("train").setLevel('WARNING')
+logger = logging.getLogger("train")
 
 if __name__ == '__main__':
-    opt = Conll_dataset.Options('en', 'en')
-    logging.basicConfig(filename='train.log',level=logging.DEBUG)
+    logging.basicConfig(filename='train.log', level=logging.DEBUG)
 
     dataset = Conll_dataset(opt, tag_type='ner', train=True)
     train_iter, validation_iter, test_iter = dataset.batch_iter(batch_size=32)
