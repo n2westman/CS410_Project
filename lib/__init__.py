63d458c8afd10e0a2d2e847d98d63aaf6d18830b
Nickolas Westman <westman4@illinois.edu>
2018-11-17 11:11:22 -0800

    Set up lib module

---
+++ lib/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['data', 'model', 'train', 'utils']
+
+import data
+import model
+import train
+import utils
--- train.py
+++ train.py
@@ -1,10 +1,10 @@
-from lib.data import Conll_dataset
 from itertools import count
 import os
 import numpy as np
 from config import opt, additional_args
 import logging
 import time
+import lib
 start_time = time.time()
 additional_args(opt)
 N_SAMPLES = opt.nsamples
@@ -23,10 +23,10 @@ logger = logging.getLogger("train")
 if __name__ == '__main__':
     logging.basicConfig(filename='train.log', level=logging.DEBUG)
 
-    dataset = Conll_dataset(opt, tag_type='ner', train=True)
+    dataset = lib.data.Conll_dataset(opt, tag_type='ner', train=True)
     train_iter, validation_iter, test_iter = dataset.batch_iter(batch_size=32)
 
-    # for idx, batch in enumerate(train_iter):
-    #     print(batch.labels)
-    #     print(batch.inputs_word)
-    #     exit(0)
+    for idx, batch in enumerate(train_iter):
+        print(batch.labels)
+        print(batch.inputs_word)
+        exit(0)
