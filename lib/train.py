ebc0a04f4ced50fd967dac67e8fe48398c8204e7
Nickolas Westman <westman4@illinois.edu>
2018-11-17 09:54:10 -0800

    Loading dataset correctly

--- lib/train.py
+++
@@ -1,5 +0,0 @@
-from data import Conll_dataset;
-
-if __name__ == '__main__':
-    dataset = data.Conll_dataset(opt, tag_type='ner', train=False)
-    train_iter, validation_iter, test_iter = dataset.batch_iter(batch_size=32)
---
+++ train.py
@@ -0,0 +1,9 @@
+from lib.data import Conll_dataset
+import logging
+
+if __name__ == '__main__':
+    opt = Conll_dataset.Options('en', 'en')
+    logging.basicConfig(filename='train.log',level=logging.DEBUG)
+
+    dataset = Conll_dataset(opt, tag_type='ner', train=False)
+    train_iter, validation_iter, test_iter = dataset.batch_iter(batch_size=32)
