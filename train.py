0b27a419f67d4e77a51a1dce1a1460afa9793932
Nickolas Westman <westman4@illinois.edu>
2018-11-28 12:44:18 -0800

    Print some sample data out

--- train.py
+++ train.py
@@ -13,20 +13,24 @@ N_SAMPLES = opt.nsamples
 logging.basicConfig(filename='train.log', level=logging.INFO)
 logger = logging.getLogger("train")
 
+def print_samples(n, dataset):
+    train_iter, _, _ = dataset.batch_iter(batch_size=n)
+
+    for i, batch in enumerate(train_iter):
+        sentences = [' '.join(l) for l in lib.utils.indices2words(batch.inputs_word, dataset.vocabs[0], remove_pad=True)]
+        for j, sentence in enumerate(sentences):
+            print('Example %d: %s' % (j+1, sentence))
+        break
+
 def main():
     logger.info(opt)
 
     dataset = lib.data.Conll_dataset(opt, tag_type='ner', train=True)
-    train_iter, validation_iter, test_iter = dataset.batch_iter(batch_size=32)
-
+    train_iter, validation_iter, test_iter = dataset.batch_iter(batch_size=2)
 
     wordrepr =  lib.train.build_wordrepr(opt, dataset.vocabs)
-    logger.info('Begin training active learning policy..')
 
-    # for idx, batch in enumerate(train_iter):
-    #     print(batch.labels)
-    #     print(batch.inputs_word)
-    #     exit(0)
+    print_samples(10, dataset)
 
 if __name__ == '__main__':
     main()
